cf01bbb4ed4c8543002c6b3952afff93705a832d
jiwonjeon <j.jeon339@mybvc.ca>
2024-10-28 11:25:23 -0600

    Done Websocket

--- src/server.js
+++ src/server.js
@@ -1,12 +1,46 @@
+import http from "http";
+import { WebSocketServer } from "ws";
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+const port = 3002;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.set("view engine", "pug");
-app.set("views", __dirname + "/views");
-app.use("/public", express.static(__dirname + "/public"));
+app.set("views", path.join(__dirname, "views"));
+app.use("/public", express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+const handleListen = () => console.log(`Listening on http://localhost:${port}`);
+
+const server = http.createServer(app); //http 서버 만들고
+const wss = new WebSocketServer({ server }); //http 서버 위에 wss를 만든다
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Anon";
+  console.log("Connected to Browser✅");
+  socket.on("close", () => {
+    console.log("Disconnected from Browser❌");
+  });
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg.toString("utf8"));
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+      case "nickname":
+        socket["nickname"] = message.payload;
+    }
+  });
+});
+
+server.listen(port, handleListen);
